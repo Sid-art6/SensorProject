0df238a39128f0987008df8fa158afd0e88c77aa
Sidd900 <siddhantpanigrahi75@gmail.com>
2026-01-28 22:23:53 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME = 'wafer-fault'
+MONGO_DATABASE_NAME = 'project'
+MONGO_COLLECTION_NAME = 'waferfault'
+
+
+TARGET_COLUMN = 'quality'
+MOngo_DB_URL = ''
+
+
+MODEL_FILE_NAME = 'model'
+MODEL_FILE_EXTENSION = '.pkl'
